341434ea859dbc56e63b789e71218c6a97042907
maxpme <pure_mind@list.ru>
2022-10-02 20:07:15 +0300

    creating base view

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { ReactElement } from 'react';
-import { TimerView } from './Components/TimerView';
 import { Helmet } from 'react-helmet-async';
-import timer from './store/Timer';
-import { Box, Container } from '@mui/material';
+import { Box, Card, CardMedia, Container } from '@mui/material';
 
 const Title = 'QrShare';
 
@@ -12,9 +10,30 @@ const App: React.FC = (): ReactElement => {
       <Helmet>
         <title>{Title}</title>
       </Helmet>
-      <Container maxWidth='xl' disableGutters>
-        <Box sx={{ backgroundColor: 'primary.light' }}>
-          <TimerView timer={timer} />
+      <Container maxWidth='xl' disableGutters sx={{ height: '100vh' }}>
+        <Box
+          sx={{
+            backgroundColor: 'primary.main',
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Card
+            raised
+            sx={{
+              p: 1,
+              boxSizing: 'border-box',
+              width: 0.5,
+              height: 0.5,
+              '&': { borderRadius: '20px' },
+            }}
+          >
+            <CardMedia component='img' height='140' image='' />
+          </Card>
+          {/* <TimerView timer={timer} /> */}
         </Box>
       </Container>
     </>
